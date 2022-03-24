5d1457a2270b387aa1a2bab098a460af4490df79
JackDavidWearn <wearnjack@gmail.com>
2022-03-24 14:46:57 +0000

    Updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 export MYSQL_ROOT_PASSWORD
 export SECRET_KEY
 docker stack deploy --compose-file docker-compose.yaml animal-noise-project
-docker service update --replicas 3 animal-noise-project_animal-noise-front
