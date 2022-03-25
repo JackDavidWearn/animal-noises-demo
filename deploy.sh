769267de12e589d62a5dbfb27fd9bcf8752d9a5d
JackDavidWearn <wearnjack@gmail.com>
2022-03-25 10:32:23 +0000

    Testing

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 export MYSQL_ROOT_PASSWORD
 docker stack deploy --compose-file docker-compose.yaml card-deck-stack
-docker service update --replicas 3 card-deck-stack_front-end
+docker service update --replicas 3 card-deck-stack_card-deck-front
