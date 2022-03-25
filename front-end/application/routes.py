d83d8cc5506a61b03bf1e37a7581cee777aaf43c
JackDavidWearn <wearnjack@gmail.com>
2022-03-25 11:22:58 +0000

    Testing

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -7,7 +7,7 @@ import requests
 def index():
     card_value = requests.get('http://cardvalue-api:5000/get_value')
     card_suit = requests.get('http://cardsuit-api:5000/get_suit')
-    selected_card = requests.post('http://card_api:5000/get_card', json = {"Value": card_value.text, "Suit": card_suit.text})
+    selected_card = requests.post('http://card_api:5000/get_card', json = {"value": card_value.text, "suit": card_suit.text})
     cards = Cards(card_value = card_value.text, card_suit = card_suit.text)
     db.session.add(cards)
     db.session.commit()
